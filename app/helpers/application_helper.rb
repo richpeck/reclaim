d7bb9cd6804e59617f090e3a4f1eac5240553bba
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-01-21 10:55:15 +0000

    Path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-
-  ## PDF Image View ##
-  def pdf_image_url(image, options = {})
-    options[:src] = ["file:///", Rails.root.join("app", "assets", "images", image)].join
-    tag(:img, options)
-   end
-
 end
