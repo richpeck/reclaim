9430aaade0da246a36cffb03dd169e3a74f771ae
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-01-21 08:58:49 +0000

    Wicked PDF Image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   ## PDF Image View ##
   def pdf_image_url(image, options = {})
-    options[:src] = Rails.root.join('public', image)
+    options[:src] = Rails.root.join(image)
     tag(:img, options)
    end
 
