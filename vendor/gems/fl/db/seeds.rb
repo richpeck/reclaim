12bba23810d0be89f926fd4c70b4e470921b9cf0
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-01-25 17:19:04 +0000

    Added

--- vendor/gems/fl/db/seeds.rb
+++ vendor/gems/fl/db/seeds.rb
@@ -107,9 +107,11 @@ if Dir.exists? seeds
 
         # => Random file
         file = files.sample
+        open = File.open(file)
 
         # => News
         news.featured_image.purge if Rails.env.staging? # => Removes any instances of ActiveStorage so we can add a new one
+        puts open.inspect()
         news.featured_image.attach(io: File.open(file), filename: File.basename(file), content_type: 'image/jpeg') unless news.featured_image.attached? # => Upload stored files
 
       end
